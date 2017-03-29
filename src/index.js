8a943ce8d68132010be907ea8dbdc0bd67234f9c
Keith Lockey <lockey@gmail.com>
2017-03-28 21:42:46 -0400

    first submittal

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 if (process.env.NODE_ENV !== 'production') require('./index.html')
 import './styles/screen.scss'
 
+console.log('here')
+
 const main = () => {
-  document.querySelector('h1').textContent += '?'
+  const ulist = document.querySelector('ul.listOfCheckboxes')
+  const oneList = document.querySelector('input[type=button]')
+  const div = document.querySelector('div.container')
+  const body = document.querySelector('body')
+
+  oneList.addEventListener('click', (event) => {
+    updateButton()
+  })
+
+  const updateButton = () => {
+    console.log('updateButton')
+    div.style.backgroundColor = 'red'
+    body.style.backgroundColor = 'purple'
+  }
 }
 
 document.addEventListener('DOMContentLoaded', main)
